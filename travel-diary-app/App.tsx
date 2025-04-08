0f296955aa12d730b0d35f252166261aa6b4d6c4
Giiiiiiin <miguel_malisa@dlsl.edu.ph>
2025-04-09 03:31:31 +0800

    Version 2.1

--- travel-diary-app/App.tsx
+++ travel-diary-app/App.tsx
@@ -2,12 +2,15 @@ import { SafeAreaView } from 'react-native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { styles } from './src/styles/globalStyles';
 import FlashMessage from "react-native-flash-message";
+import { GlobalContextProvider } from './src/context/globalContext';
 
 export default function App() {
   return (
     <>
       <SafeAreaView style={styles.container}>
+        <GlobalContextProvider>
           <AppNavigator />
+        </GlobalContextProvider>
       </SafeAreaView>
       <FlashMessage position="bottom" />
     </>
